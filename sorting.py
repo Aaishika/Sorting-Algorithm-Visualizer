7bacb2e4d0a41050a2e27bc4b52cede00b60af1a
Aaishika Rana <33522474+Aaishika@users.noreply.github.com>
2020-01-29 21:00:07 +0530

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -212,3 +212,54 @@ def quick_sort(nums):  # n^2
             _quick_sort(items, split_index + 1, high)
 
     _quick_sort(nums, 0, nums.get_len() - 1)
+
+    
+    
+
+def countingSort(arr, exp1): 
+
+	n = len(arr) 
+
+
+	output = [0] * (n) 
+
+	# initialize count array with 0 
+	count = [0] * (10) 
+
+	# Store count  
+	for i in range(0, n): 
+		index = (arr[i]/exp1) 
+		count[ (index)%10 ] += 1
+
+	for i in range(1,10): 
+		count[i] += count[i-1] 
+
+	#output array 
+	i = n-1
+	while i>=0: 
+		index = (arr[i]/exp1) 
+		output[ count[ (index)%10 ] - 1] = arr[i] 
+		count[ (index)%10 ] -= 1
+		i -= 1
+
+	i = 0
+	for i in range(0,len(arr)): 
+		arr[i] = output[i] 
+
+# Radix Sort 
+def radixSort(arr): 
+
+	max1 = max(arr) 
+
+ 
+	exp = 1
+	while max1/exp > 0: 
+		countingSort(arr,exp) 
+		exp *= 10
+
+arr = [ 70, 475, 795, 9, 872, 284, 9, 669] 
+radixSort(arr) 
+
+for i in range(len(arr)): 
+	print(arr[i]), 
+
